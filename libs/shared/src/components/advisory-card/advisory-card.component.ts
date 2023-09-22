ac6d13ec5745e7bba36087303f5b28b67be99ce5
Oscar Viquez <techviquez@gmail.com>
2023-09-22 19:09:55 -0400

    feature(002): add button component into advisory card component, fix styling for advisory component

--- libs/shared/src/components/advisory-card/advisory-card.component.ts
+++ libs/shared/src/components/advisory-card/advisory-card.component.ts
@@ -2,27 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdvisoryCard } from '../../models/advisorycard';
 import { DropDownAdvisoryComponent } from '../drop-down-advisory/drop-down-advisory.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
    selector: 'lib-advisory-card',
    standalone: true,
-   imports: [CommonModule, DropDownAdvisoryComponent],
+   imports: [CommonModule, DropDownAdvisoryComponent, ButtonComponent],
    templateUrl: './advisory-card.component.html',
    styleUrls: ['./advisory-card.component.scss']
 })
-export class AdvisoryCardComponent implements OnInit{
+export class AdvisoryCardComponent{
    alerts: AdvisoryCard[] = [
       new AdvisoryCard("/warning.svg", 'Warning alert', "Five Points Station", 'Elevator Malfunction'),
       new AdvisoryCard("/check.svg", 'Okay alert', "Five Points Station", 'Southbound Operation'),
       new AdvisoryCard("/alert.svg", 'Red alert', "Five Points Station", 'North Springs Trains Delay')
    ]
-
-   constructor(){}
-
-
-   ngOnInit(): void {
-      
-     
-   }
-
 }
