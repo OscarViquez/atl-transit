30e02cccdd884ce0b716c5b431fa79d4228ffd3d
Oscar Viquez <techviquez@gmail.com>
2023-10-20 13:45:42 -0400

    push code that was accidentally deleted when migrating views folder to smart library

--- libs/stations/smart/src/lib/views/train-arrival-page/train-arrival-page.component.ts
+++ libs/stations/smart/src/lib/views/train-arrival-page/train-arrival-page.component.ts
@@ -1,13 +1,29 @@
 /* eslint-disable @nx/enforce-module-boundaries */
-import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OnInit } from '@angular/core';
+//component import
 import { TrainArrivalPage } from '../../models';
-import { HeroMock, TabMock } from 'shared';
-
-
+import { CardComponent, HeroMock, TabMock } from 'shared';
+import { HeroComponent } from 'shared';
+import { TabComponent } from 'shared';
+import {LoadingSkeletonComponent} from 'shared'
+import { Store } from '@ngrx/store';
+import { generalStationActions, userLocationAction } from  'libs/+state/src/lib/state'
+import { StationStateInterface, UserStateInterface } from 'libs/+state/src/lib/models';
+import { SavedStationsCardComponent, TrainStaion, TrainStationCardComponent } from 'stations-ui';
 @Component({
    selector: 'lib-train-arrival-page',
-   standalone: false,
+   standalone: true,
+   imports: [
+      CommonModule,
+      CardComponent,
+      TrainStationCardComponent,
+      HeroComponent,
+      TabComponent,
+      SavedStationsCardComponent,
+      LoadingSkeletonComponent
+   ],
    templateUrl: './train-arrival-page.component.html',
    styleUrls: ['./train-arrival-page.component.scss']
 })
@@ -16,8 +32,16 @@ export class TrainArrivalPageComponent implements OnInit {
       tab: TabMock,
       header: HeroMock,
    };
+   trainData!: TrainStaion[];
+   
 
-   ngOnInit() {
-      console.log('Train Arrival Page Component');
+   constructor(
+      private state: Store<UserStateInterface>, 
+      private stationStore: Store<StationStateInterface>) {}
+
+    ngOnInit() {
+      this.state.dispatch(userLocationAction.location());
+      this.stationStore.dispatch(generalStationActions.stationLocate())
+      this.state.dispatch(userLocationAction.locateUserFailure({message: 'Yeah I tried'}))
    }
 }
