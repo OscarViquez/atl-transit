bcf616e4faae39ef6b06d8359168114d14f2eeb3
Oscar Viquez <techviquez@gmail.com>
2023-11-11 12:34:06 -0500

    update arrival endpoint

--- libs/+state/src/lib/types/service.types.ts
+++ libs/+state/src/lib/types/service.types.ts
@@ -2,7 +2,7 @@
 export enum EndpointResponse {
     PREDICTIONS_RESPONSE = 'http://labs.itsmarta.com/signpost/predictions',
     SIGNPOST_TRAIN_RESPONSE = 'http://labs.itsmarta.com/signpost/trains',
-    RAIL_ARRIVAL_TIMES_RESPONSE = 'https://atl-transit-community-proxy.cyclic.app/',
+    RAIL_ARRIVAL_TIMES_RESPONSE = 'https://atl-transit-community-proxy.cyclic.app/arrivals',
     GENERAL_STATION_RESPONSE = 'assets/jsons/marta.trains.json',
 
 }
