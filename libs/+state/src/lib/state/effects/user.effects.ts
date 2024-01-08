ef0a2ae0247d0bda955eeaf95e2f9c68ae31880b
Oscar Viquez <techviquez@gmail.com>
2024-01-08 11:56:09 -0500

    refactor: Update Effects, Actions. Comments on Reducer
        This commit adds new changes to Effects but also some additions to Actions and Reducer for next action item.
    
        - Add comments to Actions and Effects
        - Removed unused Effects
        - Comments on reducer
        - Remove unused or non existent imports

--- libs/+state/src/lib/state/effects/user.effects.ts
+++ libs/+state/src/lib/state/effects/user.effects.ts
@@ -4,7 +4,6 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { of } from 'rxjs';
 import { catchError, map, concatMap, withLatestFrom } from 'rxjs/operators';
-
 import { StationState, UserLocationInfo, UserState } from '../../models';
 import { UserService } from '../../services/user-location/user.service';
 import { arrivalMappingActions, userLocationAction, userTrainStations } from '../actions';
@@ -31,6 +30,7 @@ export class UserEffects {
       )
    );
 
+   // TODO: DELETE this after refactoring or reorganizing adapters. Adapter method should be used in selectors
    loadMappedData$ = createEffect(() =>
       this.actions$.pipe(
          ofType(arrivalMappingActions.arrivalMappingSuccess),
--- libs/+state/src/lib/state/reducers/station.reducer.ts
+++ libs/+state/src/lib/state/reducers/station.reducer.ts
@@ -1,60 +1,69 @@
-import { createReducer, on, State } from '@ngrx/store';
-import { generalStationActions, arrivalMappingActions, arrivalResponseActions, amenitiesActions, stationScheduleActions } from '../actions';
-import { StationState  } from '../../models';
-import { Action } from 'rxjs/internal/scheduler/Action';
-
+import { createReducer, on } from '@ngrx/store';
+import {
+   generalStationActions,
+   arrivalMappingActions,
+   arrivalResponseActions,
+   amenitiesActions,
+   stationScheduleActions
+} from '../actions';
+import { StationState } from '../../models';
 
 export const initalArrivalState: StationState = {
-    jsonStations: [], 
-    arrivalData: [], 
-    railArrivalData: [], 
-    loading: true, 
-    amenities: [], 
-    stationSchedule: [],
-    error: ''
-}
+   jsonStations: [],
+   arrivalData: [],
+   railArrivalData: [],
+   // TODO:
+   // * Either we sperate the loading state into a seperate reducer,
+   // * or we just use the loading state from the user reducer.
+   // * So we can load all data that doesnt require user location first
+   loading: true,
+   amenities: [],
+   stationSchedule: [],
+   error: ''
+};
 
 export const stationReducer = createReducer(
-    initalArrivalState, 
-    on(generalStationActions.generalStationSuccess, (state, action) => ({
-        ...state, 
-        jsonStations: action.generalStations,
-    })), 
-    on(generalStationActions.generalStationFailure, (state, action) => ({
-        ...state, 
-        error: action.message
-    })), 
-    on(arrivalResponseActions.arrivalResponseSuccess, (state, action) => ({
-        ...state, 
-        arrivalData: action.arrivalResponse,
-        loading: false
-    })), 
-    on(arrivalResponseActions.arrivalResponseFailure, (state, action) => ({
-        ...state, 
-        error: action.message
-    })), 
-    on(arrivalMappingActions.arrivalMappingSuccess, (state, action) => ({
-        ...state, 
-        railArrivalData: action.arrivalsMapped
-    })), 
-    on(arrivalMappingActions.arrivalMappingFailure, (state, action) => ({
-        ...state, 
-        error: action.message
-    })), 
-    on(amenitiesActions.amenitiesResponseSuccess, (state, action) => ({
-        ...state, 
-        amenities: action.amenities
-    })), 
-    on(amenitiesActions.amenitiesResponseFailure, (state, action) => ({
-        ...state, 
-        error: action.message
-    })), 
-    on(stationScheduleActions.stationScheduleResponseSuccess, (state, action) => ({
-        ...state, 
-        stationSchedule: action.stationSchedule
-    })), 
-    on(stationScheduleActions.stationScheduleResponseFailure, (state, action) => ({
-        ...state, 
-        error: action.message
-    }))
-)
+   initalArrivalState,
+   on(generalStationActions.generalStationSuccess, (state, action) => ({
+      ...state,
+      jsonStations: action.generalStations
+   })),
+   on(generalStationActions.generalStationFailure, (state, action) => ({
+      ...state,
+      error: action.message
+   })),
+   on(arrivalResponseActions.arrivalResponseSuccess, (state, action) => ({
+      ...state,
+      arrivalData: action.arrivalResponse,
+      loading: false
+   })),
+   on(arrivalResponseActions.arrivalResponseFailure, (state, action) => ({
+      ...state,
+      error: action.message,
+      loading: false
+   })),
+   on(arrivalMappingActions.arrivalMappingSuccess, (state, action) => ({
+      ...state,
+      railArrivalData: action.arrivalsMapped
+   })),
+   on(arrivalMappingActions.arrivalMappingFailure, (state, action) => ({
+      ...state,
+      error: action.message
+   })),
+   on(amenitiesActions.amenitiesResponseSuccess, (state, action) => ({
+      ...state,
+      amenities: action.amenities
+   })),
+   on(amenitiesActions.amenitiesResponseFailure, (state, action) => ({
+      ...state,
+      error: action.message
+   })),
+   on(stationScheduleActions.stationScheduleResponseSuccess, (state, action) => ({
+      ...state,
+      stationSchedule: action.stationSchedule
+   })),
+   on(stationScheduleActions.stationScheduleResponseFailure, (state, action) => ({
+      ...state,
+      error: action.message
+   }))
+);
