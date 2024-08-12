09dec597b5222fd20f364f8fbd9ac5892e880a57
Oscar Viquez <techviquez@gmail.com>
2024-08-12 17:10:00 -0400

    rename services

--- src/app/data-access/facades/facade.service.ts
+++ src/app/data-access/facades/facade.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { GeolocationService } from '../services/geolocation/geolocation.service';
 import { Observable } from 'rxjs';
-import { filterAndMapArrivalsFromFlag, filterValidItems } from '../helpers/observable-helpers';
+import { filterValidItems } from '../helpers/observable-helpers';
 import { TrainsService } from '../services/trains/trains.service';
 import { GeolocationState, UserStationTrainArrivalData } from '../models/state.interfaces';
-import { LocalStorageService } from '../services/local-storage/local-storage.service';
 import { StationsService } from '../services/stations/stations.service';
 
 @Injectable({
@@ -20,34 +19,30 @@ export class FacadeService {
   trainPageArrivals$!: Observable<UserStationTrainArrivalData>;
 
   constructor(
-    private geolocationService: GeolocationService,
-    private trainService: TrainsService,
+    private geolocate: GeolocationService,
+    private train: TrainsService,
     private station: StationsService
   ) {}
 
   getGeolocationState(): void {
-    this.geolocationService.initialize();
+    this.geolocate.initialize();
     this.geolocationState$ = filterValidItems(
-      this.geolocationService.geolocationStateSubject.asObservable(),
+      this.geolocate.geolocationStateSubject.asObservable(),
       state => !state.userSelectionMade
     );
     this.isLocationOn$ = filterValidItems(
-      this.geolocationService.isLocationOnSubject.asObservable(),
+      this.geolocate.isLocationOnSubject.asObservable(),
       isLocationOn => isLocationOn !== null
     );
     this.userLocSelectionMade$ = filterValidItems(
-      this.geolocationService.userSelectionMade$.asObservable(),
+      this.geolocate.userSelectionMade$.asObservable(),
       state => state
     );
   }
 
-  /**
-   * TODO: refactor service later to handle logic for train page and train tracker.
-   * This might mean we need to seperate the logic and do some abstracton on services methods.
-   */
   getAllTrainArrivals(): void {
-    this.trainService.fetchTrainPageArrivals();
-    this.trainPageArrivals$ = this.trainService.userStationTrainArrivalData$;
+    this.train.fetchTrainPageArrivals();
+    this.trainPageArrivals$ = this.train.userStationTrainArrivalData$;
   }
 
   getStationDetails(): void {
