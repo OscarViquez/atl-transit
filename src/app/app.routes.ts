2abd58da1a416a6676a8434554d3e080bbbab9c0
Oscar Viquez <techviquez@gmail.com>
2024-09-19 19:13:33 -0400

    support: move error pages to packages, under dir called errors.                                                                                                                                                                                                               This is only for organization and so it doesnt get confused the defecto folder for all pages of the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,29 +3,28 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'trains',
-    loadChildren: () => import('./packages/trains/trains.routes').then(r => r.TRAINS_ROUTES),
+    loadChildren: () =>
+      import('./packages/trains/trains.routes').then(r => r.TRAINS_ROUTES),
   },
   {
     path: 'stations',
-    loadChildren: () => import('./packages/stations/stations.routes').then(r => r.STATION_ROUTES),
+    loadChildren: () =>
+      import('./packages/stations/stations.routes').then(r => r.STATION_ROUTES),
   },
   {
     path: 'search',
-    loadChildren: () => import('./packages/search/search.routes').then(r => r.SEARCH_ROUTES),
+    loadChildren: () =>
+      import('./packages/search/search.routes').then(r => r.SEARCH_ROUTES),
     pathMatch: 'full',
   },
   {
     path: 'alerts',
     loadComponent: () =>
-      import('./pages/under-construction-page/under-construction-page.component').then(
-        c => c.UnderConstructionPageComponent
-      ),
+      import('@atl-transit/errors').then(c => c.UnderConstructionPageComponent),
   },
   {
     path: 'menu',
     loadComponent: () =>
-      import('./pages/under-construction-page/under-construction-page.component').then(
-        c => c.UnderConstructionPageComponent
-      ),
+      import('@atl-transit/errors').then(c => c.UnderConstructionPageComponent),
   },
 ];
---
+++ src/app/packages/errors/index.ts
@@ -0,0 +1,3 @@
+export * from './error-page/error-page.component';
+export * from './page-not-found/page-not-found.component';
+export * from './under-construction-page/under-construction-page.component';
